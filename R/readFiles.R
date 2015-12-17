356c6e24ff32753ae4903810b29bb1a1ec470a1a
jasenfinch <jsf9@aber.ac.uk>
2015-12-17 16:15:40 +0000

    Added readFiles documentation

--- R/readFiles.R
+++ R/readFiles.R
@@ -1,7 +1,16 @@
 #' Read and process mulitple data files
+#' @name readFiles
+#' @description Apply spectral binning on multiple data files.
+#' @param files A vector of converted data file paths
+#' @param dp An integer denoting the number of decimal places for spectral binning
+#' @param scans A vector of scan numbers that should be retrieved 
+#' @param sranges A list of vectors containing the scan events present.
+#' @param modes A vector of strings denoting mode names including the order in which the scan events occur.
+#' @param nCores The number of cores on which to parallel process.
+#' @return A list containing peak lists for the relevant scans with combined scan ranges for each present mode in the data file. 
+#' @author Jasen Finch
 #' @export
-readFiles <- 
-	function(files,dp,scans,sranges=list(c(50,1000)),modes=c("p","n"),nCores=2){ # for data collected in both modes
+readFiles <- function(files,dp,scans,sranges=list(c(50,1000)),modes=c("p","n"),nCores=2){ # for data collected in both modes
   if(nCores<2){
     pl <- lapply(files,sampProcess,scans=scans,dp=dp,sranges=sranges,modes=modes)
     # split modes
