15a6333d6e5e88daf258d170a087676ffd0b4dd3
jasenfinch <jsf9@aber.ac.uk>
2015-12-17 15:55:53 +0000

    Added massMat documentation

--- R/massMat.R
+++ R/massMat.R
@@ -1,8 +1,12 @@
-#' to build intensity matrix for individal samples or all samples
+#' build an intensity matrix for supplied peak list
+#' @name massMat
+#' @description 
+#' @param x A peak list containing peak matrices for each scan of equal row length
+#' @return An intensity matrix with as many rows as peak matrices in peak list
+#' @author Jasen Finch
 #' @export
 
-massMat <- 
-	function(x){ 
+massMat <- function(x){ 
 	z <- data.frame(x[1])
 	mat <- matrix(0,nrow=length(x),ncol=length(z[,1]))
 	for (i in 1:length(x)){
