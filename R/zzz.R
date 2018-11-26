dd505d0b2dfbd4b538098819095b2957e011063c
jasenfinch <jsf9@aber.ac.uk>
2018-11-26 14:32:15 +0000

    made metaboData install OS specific

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,13 @@
 
 .onLoad <- function(libname,pkgname) {
 	if (!requireNamespace("metaboData", quietly = TRUE)) {
-		install.packages("metaboData",repos = c('https://cloud.r-project.org',"https://aberhrml.github.io/drat/"),type = 'source')
+		
+		if (Sys.info()['sysname'] != 'Linux') {
+			type <- 'both'
+		} else {
+			type <- 'source'
+		}
+		
+		install.packages("metaboData",repos = c('https://cloud.r-project.org',"https://aberhrml.github.io/drat/"),type = type)
 	}
 }
