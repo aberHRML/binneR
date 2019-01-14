0c8f5e1328838720a155b569c93cfa06e25f1da0
jasenfinch <jsf9@aber.ac.uk>
2019-01-14 12:47:32 +0000

    fixed binCentrality() bug when decimal places other than 2 are used. Changed version to v2.0.7

--- R/binCentrality.R
+++ R/binCentrality.R
@@ -3,6 +3,6 @@
 binCentrality <- function(mz,intensity,dp = 2){
     intensity <- round(intensity)
     mzs <- unlist(map2(mz,intensity,~{rep(.x,.y)}))
-    centrality <- 1 - abs(mean(mzs) - round(mzs,2)[1])/(1*10^-dp/2)
+    centrality <- 1 - abs(mean(mzs) - round(mzs,dp)[1])/(1*10^-dp/2)
     return(centrality)
 }
