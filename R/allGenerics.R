bcf490f7f46dc324171eb661b5b5d5bb33981f77
jasenfinch <jsf9@aber.ac.uk>
2020-12-06 21:11:33 +0000

    Fixed declation of generics

--- R/allGenerics.R
+++ R/allGenerics.R
@@ -1,73 +1,61 @@
 
-setGeneric("spectralBinning", function(x) {
-    standardGeneric("spectralBinning")
-})
+setGeneric("spectralBinning", function(x)
+	standardGeneric("spectralBinning"))
 
-setGeneric("ss", function(x) {
-	standardGeneric("ss")
-})
+setGeneric("ss", function(x)
+	standardGeneric("ss"))
 
 #' @rdname results
-setGeneric("sampleInfo", function(x) {
-    standardGeneric("sampleInfo")
-})
+setGeneric("sampleInfo", function(x)
+	standardGeneric("sampleInfo"))
 
 #' @rdname results
-setGeneric("binnedData", function(x) {
-    standardGeneric("binnedData")
-})
+setGeneric("binnedData", function(x)
+	standardGeneric("binnedData"))
 
 #' @rdname results
-setGeneric("accurateData", function(x) {
-    standardGeneric("accurateData")
-})
+setGeneric("accurateData", function(x)
+	standardGeneric("accurateData"))
 
 #' @rdname plotBin
-setGeneric('plotBin',function(x,bin,type = c('all','cls','sample')) {
-	standardGeneric('plotBin')
-})
+setGeneric('plotBin',
+					 function(x,bin,
+					 				 type = c('all','cls','sample'))
+					 	standardGeneric('plotBin'))
 
 #' @rdname plotChromatogram
-setGeneric('plotChromatogram',function(x){
-	standardGeneric('plotChromatogram')
-})
+setGeneric('plotChromatogram',function(x)
+	standardGeneric('plotChromatogram'))
 
 #' @rdname plotTIC
-setGeneric('plotTIC',function(x, by = 'injOrder', colour = 'block'){
-	standardGeneric('plotTIC')
-})
+setGeneric('plotTIC',
+					 function(x, by = 'injOrder', colour = 'block')
+	standardGeneric('plotTIC'))
 
 #' @rdname plotFingerprint
-setGeneric('plotFingerprint',function(x){
-	standardGeneric('plotFingerprint')
-})
+setGeneric('plotFingerprint',function(x)
+	standardGeneric('plotFingerprint'))
 
 #' @rdname parameters
-setGeneric('scans',function(x){
-	standardGeneric('scans')
-})
+setGeneric('scans',function(x)
+	standardGeneric('scans'))
 
 #' @rdname parameters
-setGeneric('scans<-',function(x,value){
-	standardGeneric('scans<-')
-})
+setGeneric('scans<-',function(x,value)
+	standardGeneric('scans<-'))
 
 #' @rdname parameters
-setGeneric('cls',function(x){
-	standardGeneric('cls')
-})
+setGeneric('cls',function(x)
+	standardGeneric('cls'))
 
 #' @rdname parameters
-setGeneric('cls<-',function(x,value){
-	standardGeneric('cls<-')
-})
+setGeneric('cls<-',function(x,value)
+	standardGeneric('cls<-'))
 
 #' @rdname plotPurity
-setGeneric('plotPurity',function(x,histBins = 30){
-	standardGeneric('plotPurity')
-})
+setGeneric('plotPurity',function(x,histBins = 30)
+	standardGeneric('plotPurity'))
 
 #' @rdname plotCentrality
-setGeneric('plotCentrality',function(x,histBins = 30){
-	standardGeneric('plotCentrality')
-})
+setGeneric('plotCentrality',function(x,histBins = 30)
+	standardGeneric('plotCentrality'))
