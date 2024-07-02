82fc28f5dd55d17dbd06fccaf1cb116c79dbc935
jasenfinch <jsf9@aber.ac.uk>
2024-07-02 13:19:28 +0100

    Refactor peaks and header extraction

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -4,7 +4,7 @@ use mzdata::{
     io::MzMLReader,
     params::{ParamDescribed, ParamLike},
     prelude::{ByteArrayView, SpectrumLike},
-    spectrum::{ArrayType, RefPeakDataLevel},
+    spectrum::{ArrayType, MultiLayerSpectrum, RefPeakDataLevel},
 };
 use std::fs::File;
 use std::io::{BufReader, Read, Seek};
@@ -14,6 +14,43 @@ struct Header {
     scan: usize,
     polarity: String,
     scan_filter: String,
+}
+
+fn extract_header(scan: &MultiLayerSpectrum) -> Header {
+    let header = scan.description();
+    Header {
+        scan: header.index + 1,
+        polarity: header.polarity.to_string(),
+        scan_filter: header.acquisition.scans[0]
+            .get_param_by_name("filter string")
+            .unwrap()
+            .value()
+            .to_string(),
+    }
+}
+
+fn extract_peaks(scan: &MultiLayerSpectrum) -> Robj {
+    let peaks = scan.peaks();
+
+    let mz = match peaks {
+        RefPeakDataLevel::RawData(arrays) => {
+            let array = &arrays.byte_buffer_map[&ArrayType::MZArray];
+            array.to_f64().unwrap()
+        }
+        _ => todo!(),
+    }
+    .to_vec();
+
+    let intensities = match peaks {
+        RefPeakDataLevel::RawData(arrays) => {
+            let array = &arrays.byte_buffer_map[&ArrayType::IntensityArray];
+            array.to_f32().unwrap()
+        }
+        _ => todo!(),
+    }
+    .to_vec();
+
+    data_frame!(mz = mz, intensity = intensities)
 }
 
 #[extendr]
@@ -34,38 +71,12 @@ fn parse_mzml(path: &str) -> List {
     let mut peak_data: Vec<Robj> = Vec::new();
 
     for scan in mzml_reader {
-        let peaks = scan.peaks();
+        let peaks = extract_peaks(&scan);
 
-        let mz = match peaks {
-            RefPeakDataLevel::RawData(arrays) => {
-                let array = &arrays.byte_buffer_map[&ArrayType::MZArray];
-                array.to_f64().unwrap()
-            }
-            _ => todo!(),
-        }
-        .to_vec();
+        peak_data.push(peaks);
 
-        let intensities = match peaks {
-            RefPeakDataLevel::RawData(arrays) => {
-                let array = &arrays.byte_buffer_map[&ArrayType::IntensityArray];
-                array.to_f32().unwrap()
-            }
-            _ => todo!(),
-        }
-        .to_vec();
-
-        peak_data.push(data_frame!(mz = mz, intensity = intensities));
-
-        let header = scan.description();
-        header_data.push(Header {
-            scan: header.index + 1,
-            polarity: header.polarity.to_string(),
-            scan_filter: header.acquisition.scans[0]
-                .get_param_by_name("filter string")
-                .unwrap()
-                .value()
-                .to_string(),
-        });
+        let header = extract_header(&scan);
+        header_data.push(header);
     }
     list!(
         header = header_data.into_dataframe().unwrap(),
